c4a08512560d77c7f61dbd9c8569fb65a75ba08c
Adi Putra <m.a.syaputra.94@gmail.com>
2023-11-01 10:53:34 +0700

    send revisi landing page grooming and booking

--- resources/views/grooming.blade.php
+++ resources/views/grooming.blade.php
@@ -1,12 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>GROOMING</title>
 </head>
+
 <body>
-<H1>INI HALAMAN GROOMING</H1>
+    <H1>INI HALAMAN GROOMING</H1>
+    <H1>
+        <a href="https://wa.me/08231231412?text=Halo%20nama%20saya%20nadine
+    " target="_blank"
+            rel="noopener noreferrer">Contoh link wa</a>
+    </H1>
+    <h1>
+        website ubah teks to url encode <a href="https://www.urlencoder.io/" target="_blank"
+            rel="noopener noreferrer"></a>
+    </h1>
 </body>
+
 </html>
